708742cd6bc618573097992fcee9c4d3e349249a
mukilan20020 <gnanamukilan@gmail.com>
2024-06-05 11:41:18 +0530

    footer completed

--- src/Layout/TvShows.jsx
+++ src/Layout/TvShows.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import Footer from '../Components/Footer/Footer'
 
 const TvShows = () => {
   return (
     <div>
       <Navbar/>
+      <Footer/>
       TV Shows
     </div>
   )
