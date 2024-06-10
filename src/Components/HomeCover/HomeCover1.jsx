45594b89ed80c32b80d86e8c384ae100a71c9a45
shanjana <shanjana.suresh10@gmail.com>
2024-06-10 12:22:35 +0530

    Homecover completed

--- src/Components/HomeCover/HomeCover1.jsx
+++ src/Components/HomeCover/HomeCover1.jsx
@@ -12,24 +12,24 @@ const HomeCover1 = () => {
       
 <div className="Whole">
 
-    <div className="A1">
+    <div className="sec1">
 
-        <img className='z1' src={img1} alt="" />
+        <img className='homecover1' src={img1} alt="" />
 
-          <div className="A6">
-              <div className="A2">
+          <div className="sec4">
+              <div className="sec2">
 
-               <img className='z2' src={img2} alt="" />
+               <img className='homecover2' src={img2} alt="" />
                <h1>Watch in Tamil, Telugu, Kannada</h1>
                <p>A major bank heist takes an unnerving turn with a mysterious</p>
                <p>interloper known only as dark devil begins to hijack the operation.</p> 
 
-               <div className="A3">
-                  <div className="z3">
+               <div className="sec3">
+                  <div className="homecover3">
                      <FaPlay className='play'/>
                     <p>Play</p>
                     </div>
-                  <div className="z4">
+                  <div className="homecover4">
                   <IoIosInformationCircleOutline className='info'/>
 
                     <p>More Info</p>
